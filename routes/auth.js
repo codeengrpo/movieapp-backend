015b125f89d0dc4bea3891592d5477719f7f08b4
codeengrpo <evuwix@gmail.com>
2023-07-06 08:26:31 +0200

    add user in login response

--- routes/auth.js
+++ routes/auth.js
@@ -1,21 +1,21 @@
 const router = require("express").Router();
 const User = require("../models/User");
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
 //REGISTER
 router.post("/register", async (req, res) => {
   // Check if a user already exist
-  const emailCheck = await User.findOne({ email: req.body.email })
-  if (emailCheck) return res.status(400).send('Email already exist') //if emailCheck===true
+  const emailCheck = await User.findOne({ email: req.body.email });
+  if (emailCheck) return res.status(400).send("Email already exist"); //if emailCheck===true
   // Hash password
   const salt = await bcrypt.genSalt(10);
   const hashedPassword = await bcrypt.hash(req.body.password, salt);
   const newUser = new User({
     email: req.body.email,
     password: hashedPassword,
-    isAdmin: true
+    isAdmin: true,
   });
 
   try {
@@ -29,15 +29,15 @@ router.post("/register", async (req, res) => {
 
 //LOGIN
 router.post("/login", async (req, res) => {
-  // Check user 
-  const user = await User.findOne({ email: req.body.email })
-  if (!user) return res.status(400).send('Email is not found') //if user===false
+  // Check user
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) return res.status(400).send("Email is not found"); //if user===false
   //Password is correct
   const validPassword = await bcrypt.compare(req.body.password, user.password);
-  if (!validPassword) return res.status(400).send('Password is wrong')
+  if (!validPassword) return res.status(400).send("Password is wrong");
   // Create  and assign a token
-  const token = jwt.sign({ _id: user._id }, process.env.SECRET_KEY)
-  res.header('auth-token', token).send(token);
-})
+  const token = jwt.sign({ _id: user._id }, process.env.SECRET_KEY);
+  res.header("auth-token", token).send({ token, user });
+});
 
 module.exports = router;
